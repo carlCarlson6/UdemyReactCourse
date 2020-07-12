74908cee0eb5b9e81a400667316a82aa798cb6ad
carlCarlson6 <bullet.carlos@gmail.com>
2020-07-12 18:48:26 +0200

    renamed utils formder

--- Section004-ReactBasico/basicos/src/App.js
+++ Section004-ReactBasico/basicos/src/App.js
@@ -1,7 +1,7 @@
 import React, {Fragment, useState} from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Product from './utils/Product';
+import Product from './models/Product';
 import ListProducto from './components/ListProducto';
 
 

10e22641140ed07bc8310897d99248a09dc3a09f
carlCarlson6 <bullet.carlos@gmail.com>
2020-08-05 21:39:35 +0200

    refactor task controller

--- Section015_029-MERN/server/api/controllers/TaskController.js
+++ Section015_029-MERN/server/api/controllers/TaskController.js
@@ -14,12 +14,8 @@ class TaskController {
         try {
             validateRequest(request);
 
-            const project = await this.projectServices.FindProjectById(request.body.projectId)   
-            if(!project)
-                return projectResponses.projectNotFound(response);
-
-            if(!this.projectServices.ValidateUserProjectOwnership(request.user.id, project.creator))
-                return projectResponses.projectNotOwned(response);
+            if(!(await this.FindProjectAndOwnership(request, response)))
+                return;
 
             const task = await this.taskServices.CreateTask(request.body)
             taskResponses.taskCreated(response, {task})
@@ -34,13 +30,9 @@ class TaskController {
         try {
             validateRequest(request);
 
-            const project = await this.projectServices.FindProjectById(request.body.projectId)   
-            if(!project)
-                return projectResponses.projectNotFound(response);
-
-            if(!this.projectServices.ValidateUserProjectOwnership(request.user.id, project.creator))
-                return projectResponses.projectNotOwned(response);
-
+            if(!(await this.FindProjectAndOwnership(request, response)))
+                return;
+            
             const tasks = await this.taskServices.FindProjectTasks(request.body.projectId);
             taskResponses.tasksFound(response, tasks);
         } 
@@ -54,15 +46,8 @@ class TaskController {
         try {
             validateRequest(request);
 
-            const project = await this.projectServices.FindProjectById(request.body.projectId)   
-            if(!project)
-                return projectResponses.projectNotFound(response);
-
-            if(!this.projectServices.ValidateUserProjectOwnership(request.user.id, project.creator))
-                return projectResponses.projectNotOwned(response);
-
-            if(!(await this.taskServices.FindTaskById(request.params.id)))
-                return taskResponses.taskNotFound(response);
+            if(!(await this.FindProjectOwnershipAndFindTask(request, response)))
+                return;
 
             const newTask = this.taskServices.ConstructNewTask(request.body);
             const updatedTask = await this.taskServices.UpdateTask(request.params.id, newTask);
@@ -78,16 +63,6 @@ class TaskController {
         try {
             validateRequest(request);
 
-            const project = await this.projectServices.FindProjectById(request.body.projectId)   
-            if(!project)
-                return projectResponses.projectNotFound(response);
-
-            if(!this.projectServices.ValidateUserProjectOwnership(request.user.id, project.creator))
-                return projectResponses.projectNotOwned(response);
-
-            if(!(await this.taskServices.FindTaskById(request.params.id)))
-                return taskResponses.taskNotFound(response);
-
             await this.taskServices.DeleteTask(request.params.id);
             taskResponses.taskDeleted(response);
 
@@ -96,6 +71,34 @@ class TaskController {
         catch (error) {
             errorResponse(response, 'Error while creating the task', error);
         }
+    }
+
+    async FindProjectAndOwnership(request, response) {
+        const project = await this.projectServices.FindProjectById(request.body.projectId)   
+        if(!project) {
+            projectResponses.projectNotFound(response);
+            return false;
+        }
+                
+        if(!this.projectServices.ValidateUserProjectOwnership(request.user.id, project.creator)) {
+            projectResponses.projectNotOwned(response);
+            return false;
+        }
+        
+        return true;
+    }
+
+    async FindProjectOwnershipAndFindTask(request, response) {
+        if(!(await this.FindProjectAndOwnership(request, response))) {
+            return false;
+        }
+
+        if(!(await this.taskServices.FindTaskById(request.params.id))) {
+            taskResponses.taskNotFound(response);
+            return false;
+        }
+
+        return true;
     }
 }
 

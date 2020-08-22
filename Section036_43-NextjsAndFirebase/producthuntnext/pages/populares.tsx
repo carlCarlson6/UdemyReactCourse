16696661c151fe60066d07f066b26555a0917068
carlCarlson6 <bullet.carlos@gmail.com>
2020-08-23 01:36:24 +0200

    show products by votes

--- Section036_43-NextjsAndFirebase/producthuntnext/pages/populares.tsx
+++ Section036_43-NextjsAndFirebase/producthuntnext/pages/populares.tsx
@@ -1,13 +1,36 @@
 import React, { Fragment } from 'react';
 import Layout from '../components/layout/Layout';
+import {FireBaseContext} from '../database/firebase';
+import { IProduct } from '../common/models/entities/IProduct';
+import { ProductServices } from '../logic/services/ProductServices';
+import ProductDetails from '../components/layout/ProductDetails';
 
 const Populars: React.FC = (): JSX.Element => {
+    const [products, setProducts] = React.useState<Array<IProduct>>([]);
+    const {firebase} = React.useContext(FireBaseContext);
+    
+    React.useEffect(()=>{
+        ProductServices.GetProductsByVotes(firebase, setProducts);
+    },[])
 
     return (
         <Fragment>
             <div>
                 <Layout>
-                    <h1>Populares</h1>
+                    <div className="listado-productos">
+                        <div className="contenedor">
+                            <div className="bg-white">
+                                <ul>
+                                    {products.map(product => (
+                                        <ProductDetails 
+                                            key={product.id}
+                                            product={product}
+                                        />
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </Layout>
             </div>
         </Fragment>
